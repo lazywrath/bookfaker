56fa352ee118d88abc0140af45beca47c2efea79
Camille <camille.baudras@gmail.com>
2013-07-21 14:08:38 +0200

    Prise de pari : part 1

--- application/modules/backend/controllers/ApiController.php
+++ application/modules/backend/controllers/ApiController.php
@@ -607,14 +607,6 @@ class Backend_ApiController extends Bookfaker_Controller_Backend_Action
             }
 
     }
-    
-    public function saveBetsAction(){
-        $body = $this->getRequest()->getRawBody();
-        $data = Zend_Json::decode($body);
-        
-        var_dump($data); exit;
-    }
-    
     public function matchesAction(){
         
         $repoOdds = $this->_entityManager->getRepository('Application\Model\Entities\Odds');

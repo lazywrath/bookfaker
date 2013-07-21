f222dd7c24e402218cd4664b5a2cebdd26a00b2a
Camille <camille.baudras@gmail.com>
2013-07-21 17:32:06 +0200

    Infos user accessible dans Controllers

--- library/Bookfaker/Controller/Action.php
+++ library/Bookfaker/Controller/Action.php
@@ -3,11 +3,13 @@
 class Bookfaker_Controller_Action extends Zend_Controller_Action{
     
     protected $_entityManager;
+    protected $_user;
     
     public function init() {
         parent::init();
-        
+
         $this->_entityManager = Zend_Registry::get("entityManager");
+        $this->_user = Bookfaker_Control::userInfos();
     }
 }
 

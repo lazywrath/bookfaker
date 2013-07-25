5fae01eb5e55eb9f9d4692fbd7d6fdecf63ccb6d
Camille <camille.baudras@gmail.com>
2013-07-25 11:17:23 +0200

    MAJ intégration: form login

--- library/Bookfaker/Form/LoginUser.php
+++ library/Bookfaker/Form/LoginUser.php
@@ -15,17 +15,25 @@ class Bookfaker_Form_LoginUser extends Zend_Form{
                 'label' => 'Login:',
                 'required' => true,
                 'filters'    => array('StringTrim'),
+                'placeholder' => 'Pseudo  (requis)'
             ));
+        
  
         $this->addElement('password', 'password', array(
             'label' => 'Mot de passe:',
             'required' => true,
+            'placeholder' => 'Mot de passe  (requis)'
             ));
  
         $this->addElement('submit', 'submit', array(
             'ignore'   => true,
             'label'    => 'Login',
             ));
+        
+        foreach($this->getElements() as $element)
+        {
+            $element->removeDecorator('Label');
+        }
  
     }
 }
